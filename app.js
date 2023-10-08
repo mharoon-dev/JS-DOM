4cefee1dc0e4c75fe9842b5f5123e50edd9a6daf
mharoon-dev <agar3d@gmail.com>
2023-10-09 00:00:19 +0500

    dom

---
+++ app.js
@@ -0,0 +1,20 @@
+//lamp on and off
+
+// let onBtn = document.getElementById('on')
+// let offBtn = document.getElementById('off')
+// let image = document.getElementById('image')
+
+
+
+// function on() {
+//     onBtn.style.display = 'none'
+//     offBtn.style.display = 'block'
+//     image.src = './images/with light.png'
+// }
+
+
+// function off() {
+//     offBtn.style.display = 'none'
+//     onBtn.style.display = 'block'
+//     image.src = "./images/without light (1).png"
+// }
